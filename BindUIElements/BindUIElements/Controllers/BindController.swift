bf424f9c7e9d637376002f5787fd292f763c8f51
Rafael da Silva Ferreira <rafael@swift-yah.io>
2016-10-20 16:40:19 -0200

    Setup IBOutlet in BindController
    We also setup our DisposeBag and DateFormatter instances.

--- BindUIElements/BindUIElements/Controllers/BindController.swift
+++ BindUIElements/BindUIElements/Controllers/BindController.swift
@@ -7,7 +7,37 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 final class BindController: UIViewController {
+    @IBOutlet var tapGestureRecognizer: UITapGestureRecognizer!
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var textFieldLabel: UILabel!
+    @IBOutlet var textView: BorderedTextView!
+    @IBOutlet var textViewLabel: UILabel!
+    @IBOutlet var button: UIButton!
+    @IBOutlet var buttonLabel: UILabel!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var segmentedControlLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var `switch`: UISwitch!
+    @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+    @IBOutlet var stepper: UIStepper!
+    @IBOutlet var stepperLabel: UILabel!
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var datePickerLabel: UILabel!
+
+    let dispose = DisposeBag()
+
+    lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+
+        return formatter
+    }()
 }
 
