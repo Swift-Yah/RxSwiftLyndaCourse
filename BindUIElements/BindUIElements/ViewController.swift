5f330af247144efa1d0ff9b2d84da4cb4518095c
Rafael da Silva Ferreira <rafael@swift-yah.io>
2016-10-20 12:31:23 -0200

    Create Xcode project
    This project will be used to learn about how to Bind UI elements with
    RxCocoa and RxSwift.

---
+++ BindUIElements/BindUIElements/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BindUIElements
+//
+//  Created by Rafael Ferreira on 10/20/16.
+//  Copyright © 2016 Swift Yah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
