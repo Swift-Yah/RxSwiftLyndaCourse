72c45e76bc6acc88cb39c11d071b6ba1517731e8
Rafael da Silva Ferreira <rafael@swift-yah.io>
2016-10-20 19:07:33 -0200

    Bind UI elements: Part 1
    We learn:
    - How to bind an UITextField with an UILabel using Driver.
    - How to bind an UITextView with an UILabel using driver, driveNext and
    bindNext.
    - How to bind the taps from a button using the tap control event.
    
    We do all those things using RxCocoa library.
    
    Also need do some adjustments in Storyboard file to ensure that all
    UILabels will work properly.

--- BindUIElements/BindUIElements/Controllers/BindController.swift
+++ BindUIElements/BindUIElements/Controllers/BindController.swift
@@ -22,14 +22,14 @@ final class BindController: UIViewController {
     @IBOutlet var segmentedControlLabel: UILabel!
     @IBOutlet var slider: UISlider!
     @IBOutlet var progressView: UIProgressView!
-    @IBOutlet var `switch`: UISwitch!
+    @IBOutlet var uiSwitch: UISwitch!
     @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
     @IBOutlet var stepper: UIStepper!
     @IBOutlet var stepperLabel: UILabel!
     @IBOutlet var datePicker: UIDatePicker!
     @IBOutlet var datePickerLabel: UILabel!
 
-    let dispose = DisposeBag()
+    let disposeBag = DisposeBag()
 
     lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -39,5 +39,49 @@ final class BindController: UIViewController {
 
         return formatter
     }()
+}
+
+// MARK: UIViewController functions
+
+extension BindController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUpRx()
+    }
+}
+
+// MARK: Private functions
+
+private extension BindController {
+    func setUpRx() {
+        tapGestureRecognizer.rx.event.asDriver().drive(onNext: { [unowned self] _ in
+            self.view.endEditing(true)
+        }).addDisposableTo(disposeBag)
+
+        textField.rx.text.asDriver().drive(textFieldLabel.rx.text).addDisposableTo(disposeBag)
+
+        // Using driveNext
+//        textField.rx.text.asDriver().drive(onNext: { [unowned self] in
+//            self.textFieldLabel.text = $0
+//        }).addDisposableTo(disposeBag)
+
+        // Using bindTo
+//        textField.rx.text.bindTo(textFieldLabel.rx.text).addDisposableTo(disposeBag)
+
+        textView.rx.text.asObservable().bindNext({ [unowned self] in
+            self.textViewLabel.rx.text.onNext("Character count: \($0!.characters.count)")
+//            self.textViewLabel.text = "Character count: \($0!.characters.count)"
+        }).addDisposableTo(disposeBag)
+
+        button.rx.tap.asDriver().drive(onNext: { [unowned self] _ in
+            self.buttonLabel.text! += "Tapped! "
+            self.view.endEditing(true)
+
+            UIView.animate(withDuration: 0.35) {
+                self.view.layoutIfNeeded()
+            }
+        }).addDisposableTo(disposeBag)
+    }
 }
 
