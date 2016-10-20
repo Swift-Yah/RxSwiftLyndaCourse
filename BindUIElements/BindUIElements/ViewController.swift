c0c3a9b7745f58ad98f9e92c2a365ba90e00d219
Rafael da Silva Ferreira <rafael@swift-yah.io>
2016-10-20 12:48:52 -0200

    Simplifies files generated by Xcode
    When I created a new project the Xcode always generated a lot of
    boilerplate code, we just remove this code, and rename the
    ViewController to BindController.

--- BindUIElements/BindUIElements/ViewController.swift
+++ BindUIElements/BindUIElements/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BindController.swift
 //  BindUIElements
 //
 //  Created by Rafael Ferreira on 10/20/16.
@@ -8,18 +8,6 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+final class BindController: UIViewController {
 }
 
