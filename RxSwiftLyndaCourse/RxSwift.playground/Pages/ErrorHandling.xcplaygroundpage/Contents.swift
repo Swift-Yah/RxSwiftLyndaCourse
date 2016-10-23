5d977ceb24617ab6c824d0334a43cd760108915d
Rafael da Silva Ferreira <rafael@swift-yah.io>
2016-10-23 20:24:10 -0200

    Handling errors: Part 2
    We learn how to use:
    - retry:maxAttemptCount: to define the maximum of tries the retry will
    do;
    - Driver onErrorJustReturn to return a value when an error is found
    while driving;
    - Driver onErrorDriveWith: to define other driver to use when errors is
    found;
    - Driver onErrorRecover: to define what I can do before return a new
    driver after the error occurs.

--- RxSwiftLyndaCourse/RxSwift.playground/Pages/ErrorHandling.xcplaygroundpage/Contents.swift
+++ RxSwiftLyndaCourse/RxSwift.playground/Pages/ErrorHandling.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 //: [Previous](@previous)
 
 import PlaygroundSupport
-//import RxCocoa
+import RxCocoa
 import RxSwift
 
 PlaygroundPage.current.needsIndefiniteExecution = true
@@ -54,12 +54,91 @@ exampleOf("retry") {
 
         $0.onNext(3)
         $0.onNext(4)
+
         $0.onCompleted()
 
         return NopDisposable.instance
     })
 
     sequenceThatErrors.retry().subscribe({ print($0)}).addDisposableTo(disposeBag)
+}
+
+exampleOf("retry:maxAttemptCount:") {
+    let disposeBag = DisposeBag()
+
+    var i = 1
+
+    let sequenceThatErrors = Observable<Int>.create({
+        $0.onNext(1)
+        $0.onNext(2)
+
+        if i < 5 {
+            $0.onError(ReactiveXError.test)
+
+            print("Error encountered")
+
+            i += 1
+        }
+
+        $0.onNext(3)
+        $0.onNext(4)
+
+        $0.onCompleted()
+
+        return NopDisposable.instance
+    })
+
+    sequenceThatErrors.retry(3).subscribe({ print($0) }).addDisposableTo(disposeBag)
+}
+
+exampleOf("Driver onErrorJustReturn") {
+    let disposeBag = DisposeBag()
+
+    let subject = PublishSubject<Int>()
+
+    subject.asDriver(onErrorJustReturn: 1000).drive(onNext: { print($0) }).addDisposableTo(disposeBag)
+
+    subject.onNext(1)
+    subject.onNext(2)
+
+    subject.onError(ReactiveXError.test)
+
+    subject.onNext(3)
+}
+
+exampleOf("Driver onErrorDriveWith") {
+    let disposeBag = DisposeBag()
+
+    let subject = PublishSubject<Int>()
+    let recoverySubject = PublishSubject<Int>()
+
+    subject.asDriver(onErrorDriveWith: recoverySubject.asDriver(onErrorJustReturn: 1000)).drive(onNext: { print($0) }).addDisposableTo(disposeBag)
+
+    subject.onNext(1)
+    subject.onNext(2)
+
+    subject.onError(ReactiveXError.test)
+
+    subject.onNext(3)
+
+    recoverySubject.onNext(10)
+}
+
+exampleOf("Driver onErrorRecover") {
+    let disposeBag = DisposeBag()
+
+    let subject = PublishSubject<Int>()
+
+    subject.asDriver(onErrorRecover: {
+        print("Error:", $0)
+
+        return Driver.just(1000)
+    }).drive(onNext: { print($0) }).addDisposableTo(disposeBag)
+
+    subject.onNext(1)
+    subject.onNext(2)
+
+    subject.onError(ReactiveXError.test)
 }
 
 //: [Next](@next)
